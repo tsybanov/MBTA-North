a808769a67a85400799ae3b8b6031f7a34a26e03
Maxim Tsybanov <github@tsybanov.com>
2020-07-14 18:59:04 -0400

    Add board component and present it as the main one

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Board from './board'
 
 ReactDOM.render(
     <React.StrictMode>
+        <Board />
     </React.StrictMode>,
     document.getElementById('root')
 )
