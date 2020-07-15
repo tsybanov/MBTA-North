c97ad6df689b6e8b8bc45f16bea1fc1086044957
Maxim Tsybanov <github@tsybanov.com>
2020-07-15 00:12:47 -0400

    Move prod from public/ to build/

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     entry: './src/index.js',
     output: {
-        path : path.resolve(__dirname, 'public'),
+        path : path.resolve(__dirname, 'build/'),
         filename : 'bundle.js'
     },
     module: {
